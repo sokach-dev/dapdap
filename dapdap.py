003b4852f983b8d7ce610b840714b7cb91000e78
jiahong.dong <dongjiahong@hotmail.com>
2024-03-06 14:27:06 +0800

    aotu

--- dapdap.py
+++ dapdap.py
@@ -47,24 +47,13 @@ class Browser:
         # 点击确定导入
         self.browser.find_element(By.CSS_SELECTOR, "button[data-testid='create-password-import']").click()
 
-        sleep(3)
+        sleep(2)
+        print("import wallet will")
         WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='onboarding-complete-done']"))).click()
         self.browser.find_element(By.CSS_SELECTOR, "button[data-testid='pin-extension-next']").click()
         self.browser.find_element(By.CSS_SELECTOR, "button[data-testid='pin-extension-done']").click()
         WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='popover-close']"))).click()
-
-    def get_private_key(self):
-        # key_file content like this
-        '''
-        "a" "b" "c"
-        "d" "e" "f"
-        '''
-        with open(self.private_key_path, "r") as f:
-            line = f.readlines()
-            keys = []
-            for l in line:
-                keys.append(l.strip().split(" "))
-            self.private_keys.append(keys)
+        print("import wallet success")
 
 
     def input_private_key(self, keys):
@@ -73,10 +62,10 @@ class Browser:
         # 点击确认按钮
         self.browser.find_element(By.CSS_SELECTOR, 'button[data-testid="import-srp-confirm"]').click()
 
-    def connect_wallet(self, invite_code):
+    def connect_wallet(self):
         # 点击链接钱包
         self.browser.maximize_window() # 浏览器窗口最大化，不然点不到
-        self.browser.find_element(By.CSS_SELECTOR, "button[data-bp='2001-001']").click()
+        self.browser.find_element(By.XPATH, "//div[contains(text(), 'Connect wallet & Claim')]").click()
 
         # 加载影子节点
         showShadowRoot = self.browser.find_element(By.CSS_SELECTOR, "onboard-v2").get_property('shadowRoot')
@@ -90,20 +79,64 @@ class Browser:
         WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='page-container-footer-next']"))).click()
         sleep(1)
         WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='popover-close']"))).click()
+        self.browser.close()
         self.switch_to(0)
 
+        sleep(1)
+        self.browser.execute_script("window.scrollTo(0, 300);")
+        sleep(3)
+
+        self.browser.find_element(By.XPATH, "//div[contains(text(), 'Visit the DapDap Homepage')]").click()
+        sleep(1.5)
+        self.switch_to(1)
+        self.browser.close()
+        self.switch_to(0)
+
+        sleep(2)
+        self.browser.find_element(By.XPATH, "//div[contains(text(), 'Explore DapDap Quick Onboarding')]").click()
+        sleep(1.5)
+        self.switch_to(1)
+        sleep(1)
+        self.browser.close()
+        self.switch_to(0)
+
+        sleep(2)
+        self.browser.find_element(By.XPATH, "//div[contains(text(), 'Explore DapDap QuestCampaign')]").click()
+        sleep(1.5)
+        self.switch_to(1)
+        self.browser.close()
+        self.switch_to(0)
+        sleep(1)
+
+        self.browser.refresh()
+
+        sleep(3)
+        self.browser.execute_script("window.scrollTo(0,0);")
+        sleep(2)
+
+        self.browser.find_element(By.ID, "14").click()
+        sleep(3)
+        click_ok = self.browser.find_elements(By.XPATH, "//img[@class='sc-9008961e-2 eveYpJ']")
+        if len(click_ok) > 0:
+            WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//img[@class='sc-9008961e-2 eveYpJ']"))).click()
+
+        sleep(1)
+        self.browser.execute_script("window.scrollTo(0, 300);")
+        sleep(2)
+
+        sleep(1)
+        claim_buttons = WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//button[@class='sc-b6c5a104-36 gQmfOB']")))
+        for i in range(3):
+            claim_buttons[i].click()
+            sleep(2)
+            WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//img[@class='sc-9008961e-2 eveYpJ']"))).click()
+            sleep(2)
+        print("connect wallet success")
+
+        # 点击图标进入主页
+        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-97d8b0ba-3 jyaExZ']"))).click()
         sleep(5)
 
-        # 如果有邀请码（新用户）
-        if len(self.browser.find_elements(By.CSS_SELECTOR, ".sc-fb91731b-10")) > 0:
-            inputs = self.browser.find_elements(By.CSS_SELECTOR, ".sc-fb91731b-10")
-            # 填写邀请码
-            for index in range(6):
-                inputs[index].send_keys(invite_code[index])
-            self.browser.find_element(By.CSS_SELECTOR, "img[data-nimg='1']").click()
-            sleep(4)
-            self.switch_earn_score()
-    
     def earn_score(self):
         # 点击领取积分
         self.browser.find_element(By.XPATH, "//div[contains(text(), 'Earn Rewards')]").click()
@@ -117,7 +150,7 @@ class Browser:
         self.browser.back()
 
     # 签到
-    def sign_in(self, visit_cursor, visit_connection):
+    def sign_in(self):
         # 去签到
         WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".sc-54f168e4-0"))).click()
         # 签到
@@ -125,27 +158,6 @@ class Browser:
         # 点击邀请好友
         WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Invite code')]"))).click()
         sleep(2)
-        # 获取邀请码并写入文件
-        codes = WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='sc-15cbb10a-7 qZDBh']")))
-        for code in codes:
-            visit_cursor.execute('''
-            INSERT INTO visit (visit_code) VALUES (?)
-            ''', (code.text,))
-        visit_connection.commit()
-        
-    
-    # 点击收藏
-    # def add_favorite(self):
-        # self.switch_earn_score()
-        # 进入任务
-        # WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[data-bp='100151-001']"))).click()
-
-        # 点击刷新按钮
-        #WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-bbe29444-6 bcCFJQ']"))).click()
-        # 点击提取
-        # sleep(2)
-        # WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Claim')]"))).click()
-        # self.browser.back()
     
     # 点击搜索
     def click_search(self):
@@ -155,21 +167,15 @@ class Browser:
         self.browser.back()
         
     # 探索Base页面
-    def explore(self, base_url,base_projects):
+    def explore(self, base_url, base_projects):
         # https://www.dapdap.net/quest/detail?id=29
         self.browser.get(base_url)
-        sleep(1)
-        # BNS
-        # WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(text(), 'Explore Base on DapDap')]"))).click()
-        # 点击浏览
-        sleep(3)
-        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-bbe29444-4 cqppgl']"))).click()
-        sleep(3)
-        # browser.back()
-        # sleep(1)
-        # 点击收藏3个项目
-        # WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Like at least three Dapps within Base')]"))).click()
-        sleep(1)
+        sleep(1.5)
+        # 点击跳转浏览
+        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Explore the Base homepage on DapDap to discover more about Base')]"))).click()
+        sleep(5)
+        self.back()
+        sleep(2)
         # base_projects = ["https://www.dapdap.net/dapps-details?dapp_id=74", "https://www.dapdap.net/dapps-details?dapp_id=52", "https://www.dapdap.net/dapps-details?dapp_id=39"]
         for e in base_projects[:3]:
             self.browser.get(e)
@@ -182,8 +188,6 @@ class Browser:
 
         self.browser.get(base_url)
         sleep(2)
-        # 点击提取
-        # WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Claim')]"))).click()
 
         
     def scan_bitget(self, bitget_url):
@@ -195,11 +199,6 @@ class Browser:
         self.switch_to(1)
         self.browser.close()
         self.switch_to(0)
-        # 点击刷新按钮
-        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-bbe29444-6 bcCFJQ']"))).click()
-        # 点击提取
-        # sleep(2)
-        # WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Claim')]"))).click()
         sleep(1)
         self.switch_earn_score()
 
@@ -211,17 +210,38 @@ class Browser:
         WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-5df30767-3 fxOwHV']"))).click()
         sleep(1)
         WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Enter Odyssey and enjoy gaming')]"))).click()
-        sleep(1)
+        sleep(1.5)
         self.switch_to(1)
+
+        sleep(1)
+        self.browser.execute_script("window.scrollTo(0, 1400);")
+        sleep(2)
+
+        will_scan = WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='sc-d8f42075-0 emLmJW']")))
+        for i in range(5):
+            will_scan[i].click()
+            sleep(2)
+            self.switch_to(2)
+            self.browser.close()
+            self.switch_to(1)
+            sleep(2)
+        
+        self.browser.refresh()
+
+        sleep(1)
+        self.browser.execute_script("window.scrollTo(0, 800);")
+        sleep(2)
+        for i in range(11):
+            WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-a2aeaff6-23 XXEhS']"))).click()
+            WebDriverWait(self.browser, 15).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-e10d8647-2 jmgyFp']"))).click()
+
+        sleep(1)
+        # 点击提取
+        WebDriverWait(self.browser, 15).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-a2aeaff6-20 hzciMW']"))).click()
+        sleep(2)
+
         self.browser.close()
         self.switch_to(0)
-        # 点击刷新按钮
-        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-bbe29444-6 bcCFJQ']"))).click()
-        
-        sleep(2)
-        # 点击提取
-        # WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Claim')]"))).click()
-        # sleep(1)
         self.switch_earn_score()
     
     def do_claim(self, info):
@@ -230,39 +250,45 @@ class Browser:
     def claim(self, profile_url):
         self.browser.get(profile_url)
 
+
         infos = ["Explore Base on DapDap", "Explore DapDap Odyssey Vol. 1", "Visit Bitget Wallet", "Like Your Favorite dApps and Quests", "Check-In And Invite Friends On DapDap!", "Discover With Search: Unleash What You Seek!"]
         for info in infos:
+            sleep(1)
+            self.browser.execute_script("window.scrollTo(0, 400);")
+            sleep(1)
+
             WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(text(), '{}')]".format(info)))).click()
             sleep(1)
             # 点击提取
             WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Claim')]"))).click()
             self.back()
             sleep(2)
+            self.browser.execute_script("window.scrollTo(0, 400);")
+        
+        total_pts = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-258edd1c-11 knJuWT']"))).text
+        return int(total_pts.split(" ")[0])
 
 
-def do(private_key, visit_code, visit_cursor, visit_connection):
+
+def do(private_key):
     browser = Browser(
-        '/Users/lele/develop/Selenium/hello_selenium/metamask.11.10.crx', 
+        '/Users/lele/develop/Selenium/dapdap_selenium/metamask.11.10.crx', 
         'http://127.0.0.1:1087',
-        '/Users/lele/develop/Selenium/hello_selenium/private_keys.txt'
+        '/Users/lele/develop/Selenium/dapdap_selenium/private_keys.txt'
         )
-    browser.navigate("https://www.dapdap.net")
+    browser.navigate("https://www.dapdap.net/invite/Devbose")
     browser.switch_to(1)
-    # acoustic neutral poverty half acid return flag cat empower they arrow alarm
-    # key = ["acoustic", "neutral", "poverty", "half", "acid", "return", "flag", "cat", "empower", "they", "arrow", "alarm"]
 
     browser.import_wallet(private_key)
     browser.switch_to(0)
-    # 2e13b2
-    # visite_code = ["2", "e", "1", "3", "b", "2"]
     sleep(3)
     # 点击链接钱包按钮
-    browser.connect_wallet(visit_code)
+    browser.connect_wallet()
     sleep(1)
     # 进入赚钱页面
     browser.earn_score()
     # 签到
-    browser.sign_in(visit_cursor, visit_connection)
+    browser.sign_in()
     browser.back()
     sleep(1)
     # 浏览游戏
@@ -274,17 +300,14 @@ def do(private_key, visit_code, visit_cursor, visit_connection):
     browser.explore("https://www.dapdap.net/quest/detail?id=29", base_projects)
     sleep(1)
     browser.click_search()
-    # browser.add_favorite()
-    browser.claim("https://www.dapdap.net/profile")
+    return browser.claim("https://www.dapdap.net/profile")
 
 
 if __name__ == '__main__':
     connection = sqlite3.connect('dapdap.db')
     cursor = connection.cursor()
-    visit_connection = sqlite3.connect('dapdap_visit.db')
-    visit_cursor = connection.cursor()
 
-    for i in range(1, 30000):
+    for i in range(1,30000):
         # 选择一个未使用过的wallet
         cursor.execute('''
         SELECT * FROM wallet WHERE used = 0 LIMIT 1
@@ -293,41 +316,25 @@ if __name__ == '__main__':
         cursor.execute('''
         UPDATE wallet SET used = 1 WHERE id = ?
         ''', (wallet[0],))
-
-        # 查找一个邀请码
-        visit_cursor.execute('''
-        SELECT * FROM visit WHERE used = 0 LIMIT 1
-        ''')   
-        visit = visit_cursor.fetchone()
-        visit_cursor.execute('''
-        UPDATE visit SET used = 1 WHERE id = ?
-        ''', (visit[0],))
-        visit_connection.commit()
-
+        connection.commit()
         mnemonic = wallet[1].strip().split(" ")
-        # abic => 'a' 'b' 'i' 'c'
-        visit_code = []
-        for v in visit[1].strip():
-            visit_code.append(v)
 
         try:
-            do(mnemonic, visit_code, visit_cursor, visit_connection)
+            pts = do(mnemonic)
+            cursor.execute('''
+            UPDATE wallet SET used = ? WHERE id = ?
+            ''', (pts, wallet[0]))
+            connection.commit()
         except Exception as e:
             print("error: ", e)
-            print("mnemonic: ", mnemonic)
-            print("visit_code: ", visit_code)
+            print("id: ", wallet[0], "mnemonic: ", mnemonic)
+            sleep(2)
             continue
 
-        print("done: ", i, " mnemonic: ", mnemonic, " visit_code: ", visit_code)
-        sleep(2)
+        print("success done: ", i, "id: ", wallet[0], " mnemonic: ", mnemonic)
+        sleep(5)
 
     cursor.close()
     connection.close()
-
-    visit_cursor.close()
-    visit_connection.close()
-
-
-
-    sleep(550)
+    print("done all")
 
