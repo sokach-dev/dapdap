f88d3495febd4b13a801461cc719fc2c5bf73379
jiahong.dong <dongjiahong@hotmail.com>
2024-03-20 10:00:45 +0800

    add more func

--- dapdap.py
+++ dapdap.py
@@ -10,17 +10,21 @@ EXTENSION_ID = "nkbihfbeogaeaoehlefnkodbefgpgknn"
 WALLET_PWD = "d12345678d"
 
 class Browser:
-    def __init__(self, extension_path, proxy_server, private_key_path):
+    def __init__(self, extension_path, proxy_server, chromederiver_path='chromedriver'):
         self.browserOption = webdriver.ChromeOptions()
         self.browserOption.add_extension(extension_path)
         self.browserOption.add_argument('--proxy-server=' + proxy_server)
         self.browserOption.add_argument('--mute-audio')
-        self.browser = webdriver.Chrome(options=self.browserOption)
-        self.private_key_path = private_key_path
-        self.private_keys = [[]]
+        service = webdriver.ChromeService(executable_path=chromederiver_path)
+        self.browser = webdriver.Chrome(options=self.browserOption, service=service)
     
     def switch_to(self, index):
         self.browser.switch_to.window(self.browser.window_handles[index])
+
+    def zoom(self):
+        # 缩放浏览器到0.7, 由于很多页面不好点，缩小一点加载的多
+        self.browser.get('chrome://settings/')
+        self.browser.execute_script('chrome.settingsPrivate.setDefaultZoom(0.7);')
 
     def navigate(self, url):
         self.browser.get(url)
@@ -63,8 +67,8 @@ class Browser:
 
     def connected_wallet(self):
         # 点击链接钱包
-        self.browser.maximize_window() # 浏览器窗口最大化，不然点不到
-        connect_buttons = self.browser.find_element(By.XPATH, "//button[contains(text(), 'Connect Wallet')]")
+        # self.browser.maximize_window() # 浏览器窗口最大化，不然点不到
+        connect_buttons = self.browser.find_elements(By.XPATH, "//button[contains(text(), 'Connect Wallet')]")
         if len(connect_buttons) == 0:
             return
         connect_buttons[0].click()
@@ -73,7 +77,7 @@ class Browser:
 
     def connect_wallet(self):
         # 点击链接钱包
-        self.browser.maximize_window() # 浏览器窗口最大化，不然点不到
+        # self.browser.maximize_window() # 浏览器窗口最大化，不然点不到
         self.browser.find_element(By.XPATH, "//button[contains(text(), 'Connect Wallet')]").click()
 
         # 加载影子节点
@@ -86,7 +90,7 @@ class Browser:
         self.browser.get("chrome-extension://{}/popup.html".format(EXTENSION_ID))
         WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='page-container-footer-next']"))).click()
         WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='page-container-footer-next']"))).click()
-        sleep(1)
+        sleep(1.5)
         WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='popover-close']"))).click()
         self.browser.close()
         self.switch_to(0)
@@ -94,7 +98,8 @@ class Browser:
 
     def earn_score(self):
         # 点击领取积分
-        # self.browser.find_element(By.XPATH, "//div[contains(text(), 'Earn Rewards')]").click()
+        self.browser.get("https://www.dapdap.net/")
+        sleep(1)
         WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(text(), 'Earn Rewards')]"))).click()
     
     def switch_earn_score(self):
@@ -120,6 +125,101 @@ class Browser:
         sleep(2)
         self.browser.back()
         
+    def explore_linea(self):
+        self.browser.get("https://www.dapdap.net/network/linea")
+        #Explore linea
+        sleep(2)
+        dapps = ["https://www.dapdap.net/dapps-details?dapp_id=111", "https://www.dapdap.net/dapps-details?dapp_id=7","https://www.dapdap.net/dapps-details?dapp_id=92"]
+        for i, dapp in enumerate(dapps):
+            self.browser.get(dapp)
+            sleep(2)
+            WebDriverWait(self.browser, 5).until(lambda x: x.find_element(By.CSS_SELECTOR,".fxOwHV svg")).click()
+            sleep(1)
+
+    def play_game(self):
+        # 移动屏幕
+        flip_match_position = self.browser.find_element(By.CSS_SELECTOR, ".fdguFR")
+        self.browser.execute_script("arguments[0].scrollIntoView(true);", flip_match_position)
+        sleep(5)
+
+        # 点击开玩
+        while int(self.browser.find_element(By.CSS_SELECTOR, ".cdKzPR").text) > 0:
+            # 点击开始按钮
+            WebDriverWait(self.browser, 10).until(lambda x: x.find_element(By.CSS_SELECTOR,".iiaOmS")).click()
+            sleep(2)
+            boxes = self.browser.find_elements(By.CSS_SELECTOR, ".fKfqQB")
+            for i in range(1, 10):
+                for ii, box in enumerate(boxes):
+                    image = box.find_element(By.CSS_SELECTOR, ".back-face").get_attribute("src")
+                    if image.find(f"{i}.jpg") > -1:
+                        print(f"image: {image}, position: {ii+1}")
+                        box.click()
+                        sleep(0.2)
+
+            try:
+                # 点击close按钮
+                WebDriverWait(self.browser, 10).until(lambda x: x.find_element(By.CSS_SELECTOR,".hdtiHS")).click()
+                # self.browser.find_element(By.CSS_SELECTOR, ".hdtiHS").click()
+            except Exception as e:
+                print(f"close error: {e}")
+                pass
+        sleep(2)
+
+    def odyssey_match(self):
+        self.browser.get("https://www.dapdap.net/odyssey/home?id=2")
+        sleep(2)
+        self.play_game()
+        self.explore_linea()
+        sleep(2)
+        self.browser.get("https://www.dapdap.net/odyssey/home?id=2")
+        sleep(2)
+        # Enter secret password
+        social_position = self.browser.find_element(By.CSS_SELECTOR, ".kWBNnk")
+        self.browser.execute_script("arguments[0].scrollIntoView(true);", social_position)
+        sleep(5)
+        try:
+            # input_secret = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(By.CSS_SELECTOR, ".kVBcWS"))
+            input_secret = self.browser.find_element(By.CSS_SELECTOR, ".kVBcWS")
+            input_secret.send_keys("DapDap X Linea")
+            self.browser.find_element(By.CSS_SELECTOR, ".hfAkLw").click()
+        except Exception as e:
+            print(f"enter secret password error: {e}")
+            pass
+
+        sleep(2)
+        # visit the linea
+        try:
+            visit =self.browser.find_element(By.XPATH, "//div[text()='Visit the Linea network page']")
+            visit.click()
+            sleep(2)
+            self.switch_to(1)
+            sleep(1.5)
+            self.browser.close()
+            self.switch_to(0)
+            visit.find_element(By.CSS_SELECTOR, ".cqRgKD").click()
+        except Exception as e:
+            print(f"visit linea error: {e}")
+            pass
+
+        # Start
+        flip_match_position = self.browser.find_element(By.CSS_SELECTOR, ".fdguFR")
+        self.browser.execute_script("arguments[0].scrollIntoView(true);", flip_match_position)
+        sleep(1)
+
+        # get enegy number
+        enegy_number = self.browser.find_element(By.CSS_SELECTOR, ".cdKzPR").text
+        print(f"enegy number: {enegy_number}")
+
+        self.play_game()
+
+        try:
+            # 点击啊claim按钮
+            self.browser.find_element(By.CSS_SELECTOR, ".goOIkB").click()
+            sleep(1)
+        except Exception as e:
+            print(f"claim error {e}")
+            pass
+
     # 探索Base页面
     def explore(self, base_url, base_projects):
         # https://www.dapdap.net/quest/detail?id=29
@@ -170,8 +270,10 @@ class Browser:
         sleep(1.5)
         self.switch_to(1)
 
-        sleep(1)
-        self.browser.execute_script("window.scrollTo(0, 1400);")
+        sleep(3.5)
+        ele = self.browser.find_element(By.XPATH, "//div[@class='sc-a2aeaff6-18 fUCWfF']")
+        self.browser.execute_script("arguments[0].scrollIntoView();", ele)
+        # self.browser.execute_script("window.scrollTo(0, 1400);")
         sleep(2)
 
         will_scan = WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='sc-d8f42075-0 emLmJW']")))
@@ -179,16 +281,19 @@ class Browser:
             will_scan[i].click()
             sleep(2)
             self.switch_to(2)
+            sleep(1.5)
             self.browser.close()
             self.switch_to(1)
-            sleep(2)
         
         self.browser.refresh()
 
         sleep(1)
-        self.browser.execute_script("window.scrollTo(0, 800);")
+        ele1 = self.browser.find_element(By.XPATH, "//div[@class='sc-a2aeaff6-4 fNAbPH']")
+        self.browser.execute_script("arguments[0].scrollIntoView();", ele1)
+        # self.browser.execute_script("window.scrollTo(0, 800);")
         sleep(2)
         for i in range(11):
+            sleep(1.5)
             WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-a2aeaff6-23 XXEhS']"))).click()
             WebDriverWait(self.browser, 15).until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-e10d8647-2 jmgyFp']"))).click()
 
@@ -231,9 +336,16 @@ def do(private_key):
     browser = Browser(
         '/Users/lele/develop/Selenium/dapdap_selenium/metamask.11.10.crx', 
         'http://127.0.0.1:1087',
-        '/Users/lele/develop/Selenium/dapdap_selenium/private_keys.txt'
+        '/Users/lele/develop/Selenium/chromedriver/chromedriver'
         )
-    browser.navigate("https://www.dapdap.net/referral/e5be34")
+
+    # 缩放浏览器到0.7
+    browser.zoom()
+
+    # 主账户
+    #browser.navigate("https://www.dapdap.net/referral/e5be34")
+    # 次账户
+    browser.navigate("https://www.dapdap.net/referral/c33d56")
     browser.switch_to(1)
 
     browser.import_wallet(private_key)
@@ -242,6 +354,8 @@ def do(private_key):
     # 点击链接钱包按钮
     browser.connect_wallet()
     sleep(1)
+    # 奥德赛游戏
+    browser.odyssey_match()
     # 进入赚钱页面
     browser.earn_score()
     # 签到
@@ -264,7 +378,8 @@ if __name__ == '__main__':
     connection = sqlite3.connect('dapdap_1.db')
     cursor = connection.cursor()
 
-    for i in range(1,30000):
+    for i in range(1,2):
+        #for i in range(1,30000):
         # 选择一个未使用过的wallet
         cursor.execute('''
         SELECT * FROM wallet WHERE used = 0 LIMIT 1
@@ -276,18 +391,25 @@ if __name__ == '__main__':
         connection.commit()
         mnemonic = wallet[1].strip().split(" ")
 
-        try:
-            pts = do(mnemonic)
-            cursor.execute('''
-            UPDATE wallet SET used = ? WHERE id = ?
-            ''', (pts, wallet[0]))
-            connection.commit()
-            print("total_pts: ", pts)
-        except Exception as e:
-            print("error: ", e)
-            print("id: ", wallet[0], "mnemonic: ", mnemonic)
-            sleep(2)
-            continue
+        # try:
+            # pts = do(mnemonic)
+            # cursor.execute('''
+            # UPDATE wallet SET used = ? WHERE id = ?
+            # ''', (pts, wallet[0]))
+            # connection.commit()
+            # print("total_pts: ", pts)
+        # except Exception as e:
+            # print("error: ", e)
+            # print("id: ", wallet[0], "mnemonic: ", mnemonic)
+            # sleep(2)
+            # continue
+
+        pts = do(mnemonic)
+        cursor.execute('''
+        UPDATE wallet SET used = ? WHERE id = ?
+        ''', (pts, wallet[0]))
+        connection.commit()
+        print("total_pts: ", pts)
 
         print("success done: ", i, "id: ", wallet[0], " mnemonic: ", mnemonic)
         sleep(5)
